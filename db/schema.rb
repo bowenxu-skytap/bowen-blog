1a968b884b48495342739511597df785ed081994
Bowen Xu <jimmyxbw@gmail.com>
2018-12-06 22:05:18 -0500

    finish blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_233255) do
+ActiveRecord::Schema.define(version: 2018_12_07_015553) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
